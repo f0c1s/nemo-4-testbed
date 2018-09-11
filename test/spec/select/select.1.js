e91cc617bc3116cc818dd17470f2239e68b4bb94
Anubhav Saini <anusaini@paypal.com>
2018-09-11 14:32:30 +0530

    Another select

--- test/spec/select/select.1.js
+++ test/spec/select/select.1.js
@@ -1,13 +1,21 @@
 const assert = require('assert');
 const takeAScreenShot = require('../../helpers/screenshot');
 
-describe('#Select One', function () {
-  it('should load a website', async function () {
+describe('#Select One Failure 1', function () {
+  it('should load select.1.html and fail while changing select value second time', async function () {
     const nemo = this.nemo;
     await nemo.driver.get(nemo.data.baseUrl + 'select.1.html');
     await nemo.view.select.select1WaitVisible(40000);
     assert.ok(await nemo.view.select.select1Present());
+
+    /*
     console.log(await nemo.view.select.select1().getText()); // All the option text
+        Please select one of the values.
+        A
+        B
+        C
+        D
+    */
 
     let actual;
     let expected = 'B';
@@ -48,3 +56,19 @@ describe('#Select One', function () {
       at <anonymous>
       at process._tickCallback (internal/process/next_tick.js:188:7)
   */
+
+
+describe('#Select One success', function () {
+  it('should load select.1.html and succeed in changing select\'s value', async function () {
+    const nemo = this.nemo;
+    await nemo.driver.get(nemo.data.baseUrl + 'select.1.html');
+    await nemo.view.select.select1WaitVisible(40000);
+    assert.ok(await nemo.view.select.select1Present());
+    let actual;
+    let expected = 'B';
+    await nemo.view.select.select1WaitVisible(3000).sendKeys(expected);
+    await takeAScreenShot(`${expected}.select.1`, nemo);
+    actual = await nemo.view.select.select1().getAttribute('value');
+    assert.equal(actual, expected);
+  });
+});
